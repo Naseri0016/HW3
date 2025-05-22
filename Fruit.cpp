2d9045b2e095f7bc795f1cc46458340bbfc7c876
Faezeh <keshavarzfaezeh85@gmail.com>
2025-05-22 19:59:36 +0330

    second commit for fruit.

--- Fruit.cpp
+++ Fruit.cpp
@@ -1,97 +1,131 @@
-#include <iostream>
-#include <stdexcept>
-#include <fstream>
-#include <sstream>
 #include "Fruit.hpp"
+#include <stdexcept>
+#include <iostream>
 
-using namespace std;
+Fruit::Fruit(std::string name)
+    : name(name), quantity(0), price(0), unit("kg")
+{}
 
-
-
-Fruit :: Fruit ( string name ) {
-
-    this->name=name;
-
-}
-Fruit::Fruit(double amount, string name, string unit, double price) {
-    this->name = name;
-    
-   
-    while (true) {
-        try {
-            setAmount(amount, unit);
-            break; 
-        } catch (invalid_argument &e) {
-            cout << e.what() << endl;
-            cout << "Enter new amount: ";
-            cin >> amount;
-            cout << "Enter new unit: ";
-            cin >> unit;
-        }
-    }
-    
-  
-    while (true) {
-        try {
-            setPrice(price);
-            break; 
-        } catch (invalid_argument &e) {
-            cout << e.what() << endl;
-            cout << "Please enter new price: ";
-            cin >> price;
-        }
-    }
-}
-
-
-  
-
-void Fruit:: setAmount (double amount ,string u )
+Fruit::Fruit(double amount, std::string name, std::string unit, double price)
+    : name(name), price(price), unit(unit)
 {
+    setAmount(amount, unit);
+}
 
-    if (amount < 0 )
+void Fruit::setAmount(double amount, std::string u)
+{
+    if (amount < 0)
+        throw std::invalid_argument("Amount must be positive!!!");
+
+    if (u != "kg" && u != "g" && u != "ton")
+        throw std::invalid_argument("Unit must be kg, g, or ton !!!");
+
+    if (u == "kg")
     {
-        throw invalid_argument("Amount most be positive !!!") ;
+        quantity = amount;
+        unit = u;
     }
-    else
+    else if (u == "g")
     {
-        if(u=="kg"){
+        quantity = amount / 1000.0;
+        unit = "kg";
+    }
+    else if (u == "ton")
+    {
+        quantity = amount * 1000.0;
+        unit = "kg";
+    }
+}
 
-            unit=u;
+void Fruit::setAmount(double amount)
+{
+    setAmount(amount, "kg");
+}
 
-            quantity = amount ;
-        }
-        if(u=="g"){
+void Fruit::setPrice(double p)
+{
+    if (p < 0)
+        throw std::invalid_argument("Price must be positive!");
+    price = p;
+}
 
-            unit ="kg";
+void Fruit::setName(const std::string &n)
+{
+    name = n;
+}
 
-            quantity=(amount/1000);
-        }
-        if(u=="ton"){
+double Fruit::getQuantity() const
+{
+    return quantity;
+}
 
-            unit = "kg";
+double Fruit::getPrice() const
+{
+    return price;
+}
 
-            quantity=amount*1000;
+std::string Fruit::getName() const
+{
+    return name;
+}
 
-        }
 
-        if(  u !="kg" || u !="g" || u !="ton")
+std::ostream& operator<<(std::ostream& os, const std::vector<Fruit>& fruits)
+{
+    for (const auto & fruit : fruits)
+    {
+        os << "Fruit Name: " << fruit.getName() 
+           << ", Price: $" << fruit.getPrice() 
+           << ", Quantity (kg): " << fruit.getQuantity() << "\n";
+    }
+
+    return os;
+}
+
+double operator-(std::vector<Fruit>& fruits, const Fruit& f1)
+{
+    bool find=true;
+    for (auto & fruit : fruits)
+    {
+        
+        if (fruit.getName() == f1.getName())
         {
-            throw invalid_argument("unit most be kg or g or ton");
+            find=false;
+
+            if (f1.getQuantity() <= fruit.getQuantity())
+            {
+                fruit.quantity -= f1.getQuantity();
+                return (fruit.getPrice() * (f1.getQuantity()-(int)f1.getQuantity()/5));
+            }
+            else
+            {
+                std::cerr << "Insufficient inventory!\n";
+                return 0.0;
+            }
         }
     }
+    if(find)
+    std::cerr << "Item not found !!!\n";
 
+    return 0.0;
 }
 
-void Fruit :: setPrice(double money)
+void operator+(std::vector<Fruit>& fruits, const Fruit& f1)
 {
-    if (money < 0 )
+    bool found = false;
+    for (auto & fruit : fruits)
     {
-        throw invalid_argument("price most be positive !!!") ;
+        if (fruit.getName() == f1.getName())
+        {
+            fruit.quantity += f1.getQuantity();
+            fruit.price = f1.getPrice();
+            found = true;
+            
+            break;
+        }
     }
-    else
+    if (!found)
     {
-        price = money ;
+        fruits.push_back(f1);
     }
-
 }
--- Fruit.hpp
+++ Fruit.hpp
@@ -1,37 +1,37 @@
-#ifndef FRUIT.HPP
-#define FRUIT.HPP
+#ifndef FRUIT_HPP
+#define FRUIT_HPP
 
-#include <iostream>
+#include <string>
 #include <vector>
 
 #include "Item.hpp"
 
-class  Fruit : public Item
+class Fruit : public Item
 {
-
     public:
-        
-        Fruit ( std :: string name ) ;
-        Fruit (double amount , std :: string name , std :: string unit  , double price ) ;
 
+        Fruit(std::string name);
+        Fruit(double amount, std::string name, std::string unit, double price);
 
-        void setAmount(double , std :: string) ;
-        void setPrice(double) ;
+        void setAmount(double amount, std::string u);
+        virtual void setAmount(double amount) override;
+        virtual void setPrice(double p) override;
+        virtual void setName(const std::string &n) override;
 
-       friend double operator - (std :: vector <Fruit> &list ,Fruit a1 ) ;
-       friend double operator + (std :: vector <Fruit > &list  , Fruit a1) ; 
-       
+        double getQuantity() const;
+        double getPrice() const;
+        std::string getName() const;
 
-        private :
-                
-            double quantity ; // amount of item
-            double price ;
-            std :: string name ;
-            std :: string unit;
+        friend std::ostream& operator << (std::ostream& os, const std::vector<Fruit>& fruits);
+        friend double operator - (std::vector<Fruit>& fruits, const Fruit& f);
+        friend void operator + (std::vector<Fruit>& fruits, const Fruit& f);
 
+    private:
 
-
-
+        double quantity;
+        double price;
+        std::string name;
+        std::string unit;
 };
 
 #endif
